f7b84c5935b9aa5b743b53743d2646a970b7adf7
Rahman E <rahman.erturk@gmail.com>
2022-11-12 13:39:47 +0100

    die Aufgabe gelöst

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import "./App.css";
 import Car from "./components/Car";
+import { useReducer } from "react";
+
+const initState = {
+  isSwitchedOn: false,
+  speed: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "switchOn/Off":
+      if (state.isSwitchedOn === false) {
+        state = {
+          ...state,
+          speed: 0,
+        };
+      }
+      return (state = {
+        ...state,
+        isSwitchedOn: !state.isSwitchedOn,
+      });
+
+    case "accelerate":
+      return (state = {
+        ...state,
+        speed: state.speed + 10,
+      });
+    case "brake":
+      if (state.speed >= 5) {
+        return (state = {
+          ...state,
+          speed: state.speed - 5,
+        });
+      } else {
+        return (state = {
+          ...state,
+          speed: 0,
+        });
+      }
+    default:
+      console.warn("unknown action");
+  }
+  return state;
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initState);
+
   return (
     <div className="App">
-      <Car />
+      <Car state={state} dispatch={dispatch} />
     </div>
   );
 }
--- src/components/Car.js
+++ src/components/Car.js
@@ -1,5 +1,23 @@
 import React from "react";
+import ReactSpeedometer from "react-d3-speedometer";
+import Button from "react-bootstrap/Button";
 
-export default function Car() {
-  return <div className="car">Make your instrument cluster here</div>;
+export default function Car({ state, dispatch }) {
+  return (
+    <div className="car">
+      {state.isSwitchedOn && (
+        <ReactSpeedometer
+          value={state.speed}
+          currentValueText={`${state.speed} Km/h`}
+        />
+      )}
+      <Button onClick={() => dispatch({ type: "switchOn/Off" })}>
+        {state.isSwitchedOn ? "Ausschalten" : "Anschalten"}
+      </Button>
+      <Button onMouseOver={() => dispatch({ type: "accelerate" })}>
+        Gas geben
+      </Button>
+      <Button onClick={() => dispatch({ type: "brake" })}>Bremsen</Button>
+    </div>
+  );
 }
